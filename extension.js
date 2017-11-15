e30c17fbd0b28135807328024416c21bb93e0118
Andreas Sander <andi@uberresearch.com>
2017-11-15 22:47:36 +0100

    [Added] Explorer context menu entry
    Added an entry in explorer context for jp(e)g and png files.

--- extension.js
+++ extension.js
@@ -13,11 +13,11 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.compressFile', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Compressing file!');
     });
 
     context.subscriptions.push(disposable);
