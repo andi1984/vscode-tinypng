5fcb0111cbd55c7b16da258df85e7a34cd1e4ebb
Andreas Sander <andreas.sander@hotmail.de>
2017-11-17 23:30:46 +0100

    [Updated] Cleanup

--- extension.js
+++ extension.js
@@ -87,9 +87,6 @@ function activate(context) {
     );
   });
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposableCompressFile = vscode.commands.registerCommand(
     "extension.compressFile",
     compressImage
